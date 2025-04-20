c58f898ded7bc9d81b5eff758e78d0a7c026351d
cosmothetrue <sxallumano@gmail.com>
2025-04-20 21:05:39 +1200

    Introducing Timers

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@onready var barrel_middle: MeshInstance3D = $TurretBase/TurretTop/BarrelMiddle
 
-func _ready() -> void:
+
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = barrel_middle.global_position
+	
